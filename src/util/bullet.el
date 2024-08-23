308ff566c2511b0a08b5cba5a14ccfb4c437ae26
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-08-23 16:38:21 -0500

    +(defun this-or-previous-uri () +  "Move point to follow bullet. Return bullet URI."
    Add a 'hode-bullet property to cover the entire bullet.

--- src/util/bullet.el
+++ src/util/bullet.el
@@ -15,16 +15,31 @@
           (hidden-start (+ (point) 1)) ;; exclude the asterisk
           (bullet-end   (+ (point) 4   ;; 4 extra characters
                            (length uri))))
+      ( put-text-property bullet-start (- bullet-end 1)
+        'hode-bullet t )
       ( add-text-properties bullet-start hidden-start
         `( font-lock-face
            ( :background ,(if uri "#00ffaa" "#777700")
              :foreground "#000000")
-           read-only t))
+           read-only 'hode-bullet))
       ( add-text-properties hidden-start (- bullet-end 1)
         '( invisible hode-uri
-           read-only t))
+           read-only 'hode-bullet))
       (forward-char (length bullet)))))
 
 (defun hode-bullet ()
   (interactive)
   (hode-insert-bullet nil))
+
+(defun this-or-previous-uri ()
+  "Move point to follow bullet. Return bullet URI."
+  (if (not (get-text-property (point) 'hode-bullet))
+      (goto-char (previous-single-property-change
+                  (point) 'hode-bullet)))
+  (beginning-of-line)
+  (let ((line-start (point))
+        (left-bracket (search-forward "["))
+        (right-bracket (search-forward "]")))
+    (forward-char 1)
+    ( buffer-substring-no-properties
+      left-bracket (- right-bracket 1))))
